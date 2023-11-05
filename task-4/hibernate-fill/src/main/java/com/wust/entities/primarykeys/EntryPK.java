dd831840208a871b6a839772f25c5527980c4373
vburmus <burmusvolod@gmail.com>
2023-11-05 19:55:51 +0100

    feat(task_4) added Entry with PK entity

---
+++ task-4/hibernate-fill/src/main/java/com/wust/entities/primarykeys/EntryPK.java
@@ -0,0 +1,19 @@
+package com.wust.entities.primarykeys;
+
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Embeddable
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class EntryPK implements Serializable {
+    private Long id;
+    private Long medicineId;
+    private Long prescriptionId;
+}
