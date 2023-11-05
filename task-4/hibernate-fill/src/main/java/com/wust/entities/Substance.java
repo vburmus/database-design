3c666b7908f227d99ab8655d7567017c61c47c4f
vburmus <burmusvolod@gmail.com>
2023-11-05 19:52:09 +0100

    feat(task_4) added Specialization, Substance entities

---
+++ task-4/hibernate-fill/src/main/java/com/wust/entities/Substance.java
@@ -0,0 +1,30 @@
+package com.wust.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Substance {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    @Enumerated
+    private Source source;
+    @ManyToMany
+    @JoinTable(name = "substance_allergy", joinColumns = @JoinColumn(name = "substance_id"),
+            inverseJoinColumns = @JoinColumn(name = "allergy_id"))
+    @ToString.Exclude
+    private Set<Allergy> allergySet;
+    @ManyToMany(mappedBy = "substances")
+    @ToString.Exclude
+    private Set<Medicine> medicines;
+}
