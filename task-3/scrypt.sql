3a139b35b20ae3a8b792e86fa245bd9afd9fd251
Daniel Borner <71730916+DBorner@users.noreply.github.com>
2023-11-08 18:34:58 +0100

    Small fixes

--- task-3/scrypt.sql
+++ task-3/scrypt.sql
@@ -3,7 +3,7 @@ USE `online_prescription` ;
 
 
 CREATE TABLE IF NOT EXISTS `online_prescription`.`pharmaceutical_form` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
@@ -20,10 +20,10 @@ CREATE TABLE IF NOT EXISTS `online_prescription`.`category` (
 ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `online_prescription`.`medicine` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
   `permit_number` VARCHAR(5) NOT NULL,
-  `name` VARCHAR(120) NOT NULL,
-  `pharmaceutical_form_id` INT NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `pharmaceutical_form_id` BIGINT NOT NULL,
   `category_symbol` VARCHAR(1) NOT NULL,
   PRIMARY KEY (`id`, `pharmaceutical_form_id`, `category_symbol`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
@@ -45,7 +45,7 @@ ENGINE = InnoDB;
 
 
 CREATE TABLE IF NOT EXISTS `online_prescription`.`user` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
   `phone_number` VARCHAR(15) NULL,
   `email` VARCHAR(255) NOT NULL,
   `login` VARCHAR(45) NOT NULL,
@@ -61,12 +61,12 @@ ENGINE = InnoDB;
 
 
 CREATE TABLE IF NOT EXISTS `online_prescription`.`doctor` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
   `first_name` VARCHAR(45) NOT NULL,
   `last_name` VARCHAR(45) NOT NULL,
   `pesel` VARCHAR(11) NOT NULL,
   `pwz_number` VARCHAR(7) NOT NULL,
-  `user_id` INT NOT NULL,
+  `user_id` BIGINT NOT NULL,
   PRIMARY KEY (`id`, `user_id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
   UNIQUE INDEX `pwz_number_UNIQUE` (`pwz_number` ASC) VISIBLE,
@@ -84,7 +84,7 @@ CREATE TABLE IF NOT EXISTS `online_prescription`.`doctor` (
 ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `online_prescription`.`specialization` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
@@ -94,7 +94,7 @@ ENGINE = InnoDB;
 
 
 CREATE TABLE IF NOT EXISTS `online_prescription`.`substance` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   `source` ENUM("NATURAL", "SYNTHETIC") NOT NULL,
   PRIMARY KEY (`id`),
@@ -104,7 +104,7 @@ ENGINE = InnoDB;
 
 
 CREATE TABLE IF NOT EXISTS `online_prescription`.`allergy` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   `description` VARCHAR(255) NULL,
   PRIMARY KEY (`id`),
@@ -115,11 +115,11 @@ ENGINE = InnoDB;
 
 
 CREATE TABLE IF NOT EXISTS `online_prescription`.`patient` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
   `first_name` VARCHAR(45) NOT NULL,
   `last_name` VARCHAR(45) NOT NULL,
   `pesel` VARCHAR(11) NOT NULL,
-  `user_id` INT NOT NULL,
+  `user_id` BIGINT NOT NULL,
   PRIMARY KEY (`id`, `user_id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
   INDEX `fk_patient_user_idx` (`user_id` ASC) VISIBLE,
@@ -135,11 +135,11 @@ CREATE TABLE IF NOT EXISTS `online_prescription`.`patient` (
 ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `online_prescription`.`prescription` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
   `issue_date` DATETIME NOT NULL,
   `is_cancelled` TINYINT NULL,
-  `patient_id` INT NOT NULL,
-  `doctor_id` INT NOT NULL,
+  `patient_id` BIGINT NOT NULL,
+  `doctor_id` BIGINT NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_recepta_pacjent1_idx` (`patient_id` ASC) VISIBLE,
   INDEX `fk_recepta_lekarz1_idx` (`doctor_id` ASC) VISIBLE,
@@ -155,36 +155,9 @@ CREATE TABLE IF NOT EXISTS `online_prescription`.`prescription` (
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
-
-CREATE TABLE IF NOT EXISTS `online_prescription`.`entry` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `medicine_id` INT NOT NULL,
-  `prescription_id` INT NOT NULL,
-  `quantity` INT NOT NULL,
-  `dosage` VARCHAR(45) NOT NULL,
-  `annotation` VARCHAR(255) NULL,
-  PRIMARY KEY (`id`, `medicine_id`, `prescription_id`),
-  INDEX `fk_entry_prescription_idx` (`prescription_id` ASC) INVISIBLE,
-  INDEX `fk_entry_medicine_idx` (`medicine_id` ASC) INVISIBLE,
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
-  UNIQUE INDEX `medicine_id_UNIQUE` (`medicine_id` ASC) VISIBLE,
-  CONSTRAINT `fk_entry_medicine`
-    FOREIGN KEY (`medicine_id`)
-    REFERENCES `online_prescription`.`medicine` (`id`)
-    ON DELETE RESTRICT
-    ON UPDATE RESTRICT,
-  CONSTRAINT `fk_entry_prescription`
-    FOREIGN KEY (`prescription_id`)
-    REFERENCES `online_prescription`.`prescription` (`id`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-    CHECK (`quantity` > 0), 
-CHECK (LENGTH(`dosage`) > 0))
-ENGINE = InnoDB;
-
 CREATE TABLE IF NOT EXISTS `online_prescription`.`medicine_substance` (
-  `substance_id` INT NOT NULL,
-  `medicine_id` INT NOT NULL,
+  `substance_id` BIGINT NOT NULL,
+  `medicine_id` BIGINT NOT NULL,
   PRIMARY KEY (`substance_id`, `medicine_id`),
   INDEX `fk_medicine_substance_medicine_idx` (`medicine_id` ASC) INVISIBLE,
   INDEX `fk_medicine_substance_substance_idx` (`substance_id` ASC) VISIBLE,
@@ -202,8 +175,8 @@ ENGINE = InnoDB;
 
 
 CREATE TABLE IF NOT EXISTS `online_prescription`.`substance_allergy` (
-  `substance_id` INT NOT NULL,
-  `allergy_id` INT NOT NULL,
+  `substance_id` BIGINT NOT NULL,
+  `allergy_id` BIGINT NOT NULL,
   PRIMARY KEY (`substance_id`, `allergy_id`),
   INDEX `fk_substance_allergy_allergy_idx` (`allergy_id` ASC) VISIBLE,
   INDEX `fk_substance_allergy_substance_idx` (`substance_id` ASC) INVISIBLE,
@@ -220,8 +193,8 @@ CREATE TABLE IF NOT EXISTS `online_prescription`.`substance_allergy` (
 ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `online_prescription`.`doctor_specialization` (
-  `doctor_id` INT NOT NULL,
-  `specialization_id` INT NOT NULL,
+  `doctor_id` BIGINT NOT NULL,
+  `specialization_id` BIGINT NOT NULL,
   PRIMARY KEY (`doctor_id`, `specialization_id`),
   INDEX `fk_lekarz_has_specjalizacja_specjalizacja1_idx` (`specialization_id` ASC) VISIBLE,
   INDEX `fk_lekarz_has_specjalizacja_lekarz1_idx` (`doctor_id` ASC) VISIBLE,
@@ -239,7 +212,7 @@ ENGINE = InnoDB;
 
 
 CREATE TABLE IF NOT EXISTS `online_prescription`.`specialization_category` (
-  `specialization_id` INT NOT NULL,
+  `specialization_id` BIGINT NOT NULL,
   `category_symbol` VARCHAR(1) NOT NULL,
   INDEX `fk_specjalizacja_has_kategoria_kategoria1_idx` (`category_symbol` ASC) VISIBLE,
   INDEX `fk_specjalizacja_has_kategoria_specjalizacja1_idx` (`specialization_id` ASC) VISIBLE,
@@ -258,8 +231,8 @@ ENGINE = InnoDB;
 
 
 CREATE TABLE IF NOT EXISTS `online_prescription`.`patient_allergy` (
-  `patient_id` INT NOT NULL,
-  `allergy_id` INT NOT NULL,
+  `patient_id` BIGINT NOT NULL,
+  `allergy_id` BIGINT NOT NULL,
   PRIMARY KEY (`patient_id`, `allergy_id`),
   INDEX `fk_patient_allergy_allergy_idx` (`allergy_id` ASC) VISIBLE,
   INDEX `fk_patient_allergy_patient_idx` (`patient_id` ASC) VISIBLE,
@@ -277,7 +250,7 @@ ENGINE = InnoDB;
 
 
 CREATE TABLE IF NOT EXISTS `online_prescription`.`pharmacy` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   `address` VARCHAR(45) NOT NULL,
   `phone_number` VARCHAR(15) NULL,
@@ -291,12 +264,12 @@ ENGINE = InnoDB;
 
 
 CREATE TABLE IF NOT EXISTS `online_prescription`.`pharmacist` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
   `first_name` VARCHAR(45) NOT NULL,
   `last_name` VARCHAR(45) NOT NULL,
   `pesel` VARCHAR(11) NOT NULL,
   `pwzf_number` VARCHAR(8) NOT NULL,
-  `user_id` INT NOT NULL,
+  `user_id` BIGINT NOT NULL,
   PRIMARY KEY (`id`, `user_id`),
   INDEX `fk_pharmacist_user_idx` (`user_id` ASC) VISIBLE,
   CONSTRAINT `fk_pharmacist_user`
@@ -311,10 +284,48 @@ CREATE TABLE IF NOT EXISTS `online_prescription`.`pharmacist` (
 	CHECK (LENGTH(`pwzf_number`) > 0))
 ENGINE = InnoDB;
 
+CREATE TABLE IF NOT EXISTS `online_prescription`.`entry` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `medicine_id` BIGINT NOT NULL,
+  `prescription_id` BIGINT NOT NULL,
+  `quantity` INT NOT NULL,
+  `dosage` VARCHAR(45) NOT NULL,
+  `annotation` VARCHAR(255) NULL,
+  `status` ENUM("COMPLETED", "ORDERED", "CANCELLED") NOT NULL,
+  `pharmacy_id` BIGINT NOT NULL,
+  `pharmacist_id` BIGINT NOT NULL,
+  PRIMARY KEY (`id`, `medicine_id`, `prescription_id`, `pharmacy_id`, `pharmacist_id`),
+  INDEX `fk_entry_prescription_idx` (`prescription_id` ASC) INVISIBLE,
+  INDEX `fk_entry_medicine_idx` (`medicine_id` ASC) INVISIBLE,
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
+  UNIQUE INDEX `medicine_id_UNIQUE` (`medicine_id` ASC) VISIBLE,
+  INDEX `fk_entry_pharmacy1_idx` (`pharmacy_id` ASC) VISIBLE,
+  INDEX `fk_entry_pharmacist1_idx` (`pharmacist_id` ASC) VISIBLE,
+  CONSTRAINT `fk_entry_medicine`
+    FOREIGN KEY (`medicine_id`)
+    REFERENCES `online_prescription`.`medicine` (`id`)
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT,
+  CONSTRAINT `fk_entry_prescription`
+    FOREIGN KEY (`prescription_id`)
+    REFERENCES `online_prescription`.`prescription` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_entry_pharmacy1`
+    FOREIGN KEY (`pharmacy_id`)
+    REFERENCES `online_prescription`.`pharmacy` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_entry_pharmacist1`
+    FOREIGN KEY (`pharmacist_id`)
+    REFERENCES `online_prescription`.`pharmacist` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `online_prescription`.`pharmacy_pharmacist` (
-  `pharmacy_id` INT NOT NULL,
-  `pharmacist_id` INT NOT NULL,
+  `pharmacy_id` BIGINT NOT NULL,
+  `pharmacist_id` BIGINT NOT NULL,
   PRIMARY KEY (`pharmacy_id`, `pharmacist_id`),
   INDEX `fk_pharmacy_pharmacist_pharmacist_idx` (`pharmacist_id` ASC) VISIBLE,
   INDEX `fk_pharmacy_pharmacist_pharmacy_idx` (`pharmacy_id` ASC) VISIBLE,
@@ -328,33 +339,4 @@ CREATE TABLE IF NOT EXISTS `online_prescription`.`pharmacy_pharmacist` (
     REFERENCES `online_prescription`.`pharmacist` (`id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
-ENGINE = InnoDB;
-
-
-CREATE TABLE IF NOT EXISTS `online_prescription`.`prescription_realization` (
-  `pharmacy_id` INT NOT NULL,
-  `pharmacist_id` INT NOT NULL,
-  `entry_id` INT NOT NULL,
-  `status` ENUM("NEW", "COMPLETED", "ORDERED", "CANCELLED") NOT NULL,
-  `realized_quantity` INT NOT NULL,
-  PRIMARY KEY (`pharmacy_id`, `pharmacist_id`, `entry_id`),
-  INDEX `fk_prescription_realization_entry_idx` (`entry_id` ASC) VISIBLE,
-  INDEX `fk_prescription_realization_pharmacy_idx` (`pharmacy_id` ASC) VISIBLE,
-  INDEX `fk_prescription_realization_pharmacist_idx` (`pharmacist_id` ASC) VISIBLE,
-  CONSTRAINT `fk_prescription_realization_pharmacy`
-    FOREIGN KEY (`pharmacy_id`)
-    REFERENCES `online_prescription`.`pharmacy` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE CASCADE,
-  CONSTRAINT `fk_prescription_realization_entry`
-    FOREIGN KEY (`entry_id`)
-    REFERENCES `online_prescription`.`entry` (`id`)
-    ON DELETE RESTRICT
-    ON UPDATE CASCADE,
-  CONSTRAINT `fk_prescription_realization_pharmacist`
-    FOREIGN KEY (`pharmacist_id`)
-    REFERENCES `online_prescription`.`pharmacist` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE CASCADE,
-    CHECK (`realized_quantity` >= 0))
 ENGINE = InnoDB;
